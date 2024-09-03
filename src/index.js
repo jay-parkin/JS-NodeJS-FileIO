f3f1a709d6e448e836acecb42a7d0745ffde6250
jay-parkin <jay-parkin91@hotmail.com>
2024-09-03 20:03:21 +1000

    feat: fs init and .env file written

--- src/index.js
+++ src/index.js
@@ -7,3 +7,33 @@ let defaultEnv = {
 };
 
 console.log(defaultEnv);
+
+let contentToWrite = "";
+
+// PORT=3030
+// SOME_DATABASE_URL=blah blah blah
+
+// ["PORT", "SOME_DATABASE_URL", "SECRUITY_TOKEN"....]
+Object.keys(defaultEnv).forEach((envKey) => {
+  // Add a line to contentToWrite with the keyname and key value
+  contentToWrite += `${envKey}=${defaultEnv[envKey]}\n`;
+});
+
+console.log(contentToWrite);
+
+const fs = require("node:fs");
+// fs.writeFileSync(".env", contentToWrite);
+// console.log("File has been written!");
+
+console.log("Before the fs callback");
+
+// fs.writeFile(filePath, fileContents, callback);
+fs.writeFile(".env", contentToWrite, (error) => {
+  if (error) {
+    console.log("File writing had errors!");
+  } else {
+    console.log("File has been written!");
+  }
+});
+
+console.log("After the fs callback");
