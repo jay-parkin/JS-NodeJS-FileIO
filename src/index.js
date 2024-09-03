0ab74bc58cbeeecf7e1493ef9e9f341f2b4d8663
jay-parkin <jay-parkin91@hotmail.com>
2024-09-03 19:30:18 +1000

    project init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let defaultEnv = {
+  PORT: 3030,
+  SOME_DATABASE_URL: "blah blah blah",
+  SECURITY_TOKEN: "secret_token",
+  SECRET_API_KEY: "secret api key",
+  JWT_SECRET_KEY: "secret jwt token",
+};
+
+console.log(defaultEnv);
